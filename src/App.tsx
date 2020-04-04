bc31b1f9335c4ea5e1d2ee938aad245c6efa0186
Larry <lukaoma@utexas.edu>
2020-04-04 16:26:11 -0500

    app gal

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React    from 'react';
+import Favicon  from 'react-favicon';
+import './App.css';
+import icon     from './icon.png';
+import NavBar   from './pages/NavBar';
+import ShowCase from './pages/ShowCase';
+
+// import 'react-image-gallery/styles/css/image-gallery.css'
+
+function App() {
+
+    return (
+        <div className="box">
+            <Favicon url={icon}/>
+            <NavBar/>
+            <ShowCase/>
+        </div>
+    );
+}
+
+export default App;
