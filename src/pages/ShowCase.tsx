39435dd2e50500e87e625745f2d94c5df9d6ad1c
Larry <lukaoma@utexas.edu>
2020-04-06 09:21:17 -0500

    video updates :)

--- src/pages/ShowCase.tsx
+++ src/pages/ShowCase.tsx
@@ -1,13 +1,23 @@
 import React                                 from 'react';
 import ImageGallery, {ReactImageGalleryItem} from 'react-image-gallery';
 import feat                                  from '../assets/images/appSlide/Featured.png';
+import live                                  from '../assets/images/appSlide/livegif.gif';
 import local                                 from '../assets/images/appSlide/local.png';
 import splash                                from '../assets/images/appSlide/Splash.png';
+import welcome                               from '../assets/images/appSlide/welcome.png';
 
 const images: ReactImageGalleryItem[] = [
 	{
+		original: live,
+		thumbnail: live,
+	},
+	{
 		original: splash,
 		thumbnail: splash,
+	},
+	{
+		original: welcome,
+		thumbnail: welcome,
 	},
 	{
 		original: feat,
